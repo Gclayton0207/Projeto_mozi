687e005e1005f27be61337b27c5359e0888fb19a
yonirg <yonirabinovicigherman@gmail.com>
2022-02-08 19:59:37 -0300

    Validação do login e senha para ser redirecionado para a landing page

--- front/models/js/main.js
+++ front/models/js/main.js
@@ -1,4 +1,14 @@
-$('#btnEntrar').click(function (){
-    window.location.replace('https://www.youtube.com/')
+
+$('#btnEntrar').mouseenter(function (){
+    var email = $('#exampleInputEmail1').val()
+    var senha = $('#exampleInputPassword1').val()
+    console.log(email)
+   if(email ==='emailteste@gmail.com' && senha ==='senhaCorreta' ){
+    //console.log('funcionou')
+     window.location.replace('./landing_Page.html') 
+    }else{
+        alert('Email ou senha incorretos!\nPara saber quais são os dados corretos\nconsulte a documentação!')
+        
+    }
 })
 
