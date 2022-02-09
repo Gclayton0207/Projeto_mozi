8ce2164d681288edd21a45a9ff5d9ae69f5d66c6
yonirg <yonirabinovicigherman@gmail.com>
2022-02-09 15:46:40 -0300

    Acertando JS

--- front/models/js/main.js
+++ front/models/js/main.js
@@ -1,14 +1,19 @@
 
-$('#btnEntrar').mouseenter(function (){
+$('#btnEntrar').click(function (event){
+    event.preventDefault()
     var email = $('#exampleInputEmail1').val()
     var senha = $('#exampleInputPassword1').val()
-    console.log(email)
-   if(email ==='emailteste@gmail.com' && senha ==='senhaCorreta' ){
+    //console.log(email)
+   if(email ==='emailjunior@gmail.com' && senha ==='senhaCorreta' ){
     //console.log('funcionou')
-     window.location.replace('./landing_Page.html') 
-    }else{
+     window.location.replace('./perfil_Junior.html') 
+    }else if(email ==='emailmaster@gmail.com' && senha ==='senhaCorreta' ){
+        window.location.replace('./perfil_Master.html')
+    }else if(email ==='emailsenior@gmail.com' && senha ==='senhaCorreta' ){
+        window.location.replace('./perfil_Senior.html')
+    }
+    else{
         alert('Email ou senha incorretos!\nPara saber quais são os dados corretos\nconsulte a documentação!')
         
     }
 })
-
