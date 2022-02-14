5b9053361aa1d7d8ebb1d3b9299173df0012a6d7
Rodrigo William <rwps1234@gmail.com>
2022-02-14 15:01:02 -0300

    validando formulário recuperação de senha

--- front/controllers/js/esqueci-senha.js
+++ front/controllers/js/esqueci-senha.js
@@ -1,22 +1,33 @@
 const inputEmail = document.querySelector('#email');
 const msg = document.querySelector('#msg');
 const btn = document.querySelector('#enviar');
+const form =document.querySelector('#formulario');
+let mailOk;
 function email() {
     if(inputEmail.value == '') {
-        msg.style.color = 'orange'
-        msg.innerHTML = 'Por favor digite seu E-mail.'
+        msg.style.color = 'orange';
+        msg.innerText = 'Por favor digite seu E-mail.';
+        mailOk = false;
     } 
      else if(inputEmail.value.indexOf('@') > 0 && inputEmail.value.indexOf('.com') > 0) {
         msg.style.color = 'greenyellow';
-        msg.innerHTML = 'Email de recuperação enviado com sucesso.';
+        msg.innerText = `Email de recuperação enviado com sucesso.
+        Aguarde 5 segundos que você será redirecionado para página inicial`;
+        mailOk = true;
     } else{
-        msg.style.color = 'red'
-        msg.innerHTML = 'E-mail inválido.' 
+        msg.style.color = 'red';
+        msg.innerText = 'E-mail inválido.';
+        mailOk = false; 
     }
 };
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    email();
+btn.addEventListener('click', () => {
+   return email();
+});
 
+form.addEventListener('submit', (e) => {
+    if(mailOk == true) {
+     setTimeout(() => { location.replace('../../models/html/index.html')}, 5000);
+    }
+    return e.preventDefault();
 });
